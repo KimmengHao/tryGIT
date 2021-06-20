a4a285e91f62e3bc84f87fe2f3da4e88b8ed9f75
Arthit Kim <arthit@imake.co.th>
2021-06-20 19:02:56 +0700

    third

--- main.go
+++ main.go
@@ -13,16 +13,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello, New world!\n")
 }
 
-// Route declaration
-func router() *mux.Router {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler)
-	return r
-}
-
 // Initiate web server
 func main() {
-	router := router()
+	router := mux.NewRouter()
+	router.HandleFunc("/", handler)
 	headers := handlers.AllowedOrigins([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	methods := handlers.AllowedOrigins([]string{"GET", "POST", "DELETE", "PUT"})
 	origins := handlers.AllowedOrigins([]string{"*"})
