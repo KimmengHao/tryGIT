9a0422578dcd978f7d51a546a482b8c629b7774a
Arthit Kim <arthit@imake.co.th>
2021-06-20 18:56:34 +0700

    two

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"time"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -23,12 +23,8 @@ func router() *mux.Router {
 // Initiate web server
 func main() {
 	router := router()
-	srv := &http.Server{
-		Handler:      router,
-		Addr:         "127.0.0.1:9100",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-
-	log.Fatal(srv.ListenAndServe())
+	headers := handlers.AllowedOrigins([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	methods := handlers.AllowedOrigins([]string{"GET", "POST", "DELETE", "PUT"})
+	origins := handlers.AllowedOrigins([]string{"*"})
+	log.Fatal(http.ListenAndServe(":9100", handlers.CORS(headers, methods, origins)(router)))
 }
