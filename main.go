65405fd469b034b0f5fac08266494794b57ed54b
Arthit Kim <arthit@imake.co.th>
2021-06-21 08:31:26 +0700

    gg

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 // Initiate web server
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", handler)
 	headers := handlers.AllowedOrigins([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	methods := handlers.AllowedOrigins([]string{"GET", "POST", "DELETE", "PUT"})
 	origins := handlers.AllowedOrigins([]string{"*"})
-	log.Fatal(http.ListenAndServe(":9100", handlers.CORS(headers, methods, origins)(router)))
+	router.HandleFunc("/auth", handler)
+	log.Fatal(http.ListenAndServe(":9090", handlers.CORS(headers, methods, origins)(router)))
 }
