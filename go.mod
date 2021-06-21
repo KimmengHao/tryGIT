e50ecfa1ad1be36741b09e4cc6da991aed404f31
Arthit Kim <arthit@imake.co.th>
2021-06-21 09:25:13 +0700

    gg

--- go.mod
+++ go.mod
@@ -3,6 +3,20 @@ module main
 go 1.16
 
 require (
-	github.com/gorilla/handlers v1.5.1 // indirect
+	cloud.google.com/go v0.84.0 // indirect
+	cloud.google.com/go/firestore v1.5.0
+	cloud.google.com/go/storage v1.15.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/utahta/go-linenotify v0.5.0
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/oauth2 v0.0.0-20210615190721-d04028783cf1 // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	google.golang.org/api v0.48.0
+	google.golang.org/genproto v0.0.0-20210617175327-b9e0b3197ced // indirect
 )
--- main.go
+++ main.go
@@ -9,16 +9,35 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func Authorize(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "token:Hello, New world!")
+// EDIT THIS
+var (
+	BaseURL      = "http://localhost:9090"
+	ClientID     = "BhWus13WIhI4HI7loycM42"
+	ClientSecret = "HJHgKSrqUuhIepCzNkCx7E82RSTN1m47dqPoS1Lf6VA"
+)
+
+type numsResponseData struct {
+	UserID string  `json:"UserID"`
+	Point  float64 `json:"Point"`
+	// Jsondata string `json:"Jsondata"`
 }
 
-// Initiate web server
+func Authorize(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "token:Authorize")
+}
+
+func Callback(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "token:callabcak")
+}
+
 func main() {
 	router := mux.NewRouter()
 	headers := handlers.AllowedOrigins([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	methods := handlers.AllowedOrigins([]string{"GET", "POST", "DELETE", "PUT"})
 	origins := handlers.AllowedOrigins([]string{"*"})
 	router.HandleFunc("/auth", Authorize)
+	router.HandleFunc("/callback", Callback)
 	log.Fatal(http.ListenAndServe(":9090", handlers.CORS(headers, methods, origins)(router)))
 }
